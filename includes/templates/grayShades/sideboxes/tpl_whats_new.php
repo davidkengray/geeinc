e62dd2fb51dc6f17a3e0f038198d2be0f3aa4cdc
Julie May <davidkengray@gmail.com>
2013-06-09 01:02:59 -0600

    New publication added.

--- includes/templates/grayShades/sideboxes/tpl_whats_new.php
+++ includes/templates/grayShades/sideboxes/tpl_whats_new.php
@@ -9,9 +9,10 @@
  */
   $content = "";
   $content .= '<div class="sideBoxContent">';
-  $content .= '<p class="sideBoxText littleText"><a href="http://geeinc.ca/pdf/BlackAndWhite-2013-05-01.pdf" class="sidelink">Get Off the Regulated Rate</a></p>';
-  $content .= '<p class="sideBoxText littleText"><a href="http://geeinc.ca/pdf/BlackAndWhite-2013-05-15.pdf" class="sidelink">Is Your Grocer Green</a></p>';
+  $content .= '<p class="sideBoxText littleText"><a href="http://geeinc.ca/pdf/RECGuide-2013-06-07.pdf" class="sidelink">Residential Energy Consumers&rsquo; Guide - June 2013</a></p>';
   $content .= '<p class="sideBoxText littleText"><a href="http://geeinc.ca/pdf/BlackAndWhite-2013-05-29.pdf" class="sidelink">Alberta has no utility debt</a></p>';
+  $content .= '<p class="sideBoxText littleText"><a href="http://geeinc.ca/pdf/BlackAndWhite-2013-05-15.pdf" class="sidelink">Is Your Grocer Green</a></p>';
+  $content .= '<p class="sideBoxText littleText"><a href="http://geeinc.ca/pdf/BlackAndWhite-2013-05-01.pdf" class="sidelink">Get Off the Regulated Rate</a></p>';
   $content .= '</div>';
 ?>
 
